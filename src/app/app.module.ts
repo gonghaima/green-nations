b3cdfafa650076a06456aa93a1638dfc9b72e1e1
gonghaima@hotmail.com <5418854188>
2017-07-17 21:53:14 +1200

    Creating the separate abstracted module - (SPA Framework) - Create the Framework Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FwModule} from '../fw/fw.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FwModule
   ],
   providers: [],
   bootstrap: [AppComponent]
