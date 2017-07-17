047571172251e6a654cfbc4b4f8ef124e74d35a4
gonghaima@hotmail.com <5418854188>
2017-07-18 00:55:39 +1200

    Developing the Framework Body class

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FrameworkBodyComponent} from './framework-body/framework-body.component';
+import {ContentComponent} from './content/content.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FrameworkBodyComponent],
+  declarations: [FrameworkBodyComponent, ContentComponent],
   exports: [FrameworkBodyComponent]
 })
 export class FwModule { }
