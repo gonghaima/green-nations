46444a827e0e92ab9952193d6556363c4272da0b
gonghaima@hotmail.com <5418854188>
2017-07-18 20:59:21 +1200

    Create the TitleBar component

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FrameworkBodyComponent} from './framework-body/framework-body.component';
 import {ContentComponent} from './content/content.component';
+import {TitleBarComponent} from './title-bar/title-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FrameworkBodyComponent, ContentComponent],
+  declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent],
   exports: [FrameworkBodyComponent]
 })
 export class FwModule { }
