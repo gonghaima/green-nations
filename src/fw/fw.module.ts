db54ee0e43818f53203165b56dd403876fa15f07
gonghaima@hotmail.com <5418854188>
2017-07-25 22:24:32 +1200

    Creating the TopBar Component

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import {FrameworkBodyComponent} from './framework-body/framework-body.component';
 import {ContentComponent} from './content/content.component';
 import {TitleBarComponent} from './title-bar/title-bar.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
 import {FrameworkConfigService} from './services/framework-config.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent],
+  declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent, TopBarComponent],
   providers:[FrameworkConfigService],
   exports: [FrameworkBodyComponent]
 })
