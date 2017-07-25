aa567efb609e0f45bbaae641fc814b6348f10987
gonghaima@hotmail.com <5418854188>
2017-07-25 21:24:31 +1200

    Creating a Framework configuration service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FrameworkConfigService, FrameworkConfigSettings} from '../fw/services/framework-config.service';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
-  title = 'app';
+  constructor(private frameworkConfigService : FrameworkConfigService) {
+    let config : FrameworkConfigSettings = {
+      socialIcons: [
+        {
+          imageFile: 'assets/social-fb-bw.png',
+          alt: 'Facebook',
+          link: 'http://www.facebook.com'
+        }, {
+          imageFile: 'assets/social-google-bw.png',
+          alt: 'Google +',
+          link: 'http://www.google.com'
+        }, {
+          imageFile: 'assets/social-twitter-bw.png',
+          alt: 'Twitter',
+          link: 'http://www.twitter.com'
+        }
+      ],
+      showLanguageSelector: true,
+      showUserControls: true,
+      showStatusBar: true,
+      showStatusBarBreakpoint: 800
+    };
+
+    frameworkConfigService.configure(config);
+  }
 }
--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import {FrameworkBodyComponent} from './framework-body/framework-body.component';
 import {ContentComponent} from './content/content.component';
 import {TitleBarComponent} from './title-bar/title-bar.component';
+import {FrameworkConfigService} from './services/framework-config.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent],
+  providers:[FrameworkConfigService],
   exports: [FrameworkBodyComponent]
 })
 export class FwModule { }
