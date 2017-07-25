3682f313c630261431e06bc834ad4dc731378c7d
gonghaima@hotmail.com <5418854188>
2017-07-26 00:23:20 +1200

    Configuration, Styling, and Responsiveness : Building Responsive Directives

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -6,12 +6,14 @@ import {TitleBarComponent} from './title-bar/title-bar.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {FrameworkConfigService} from './services/framework-config.service';
 import {StatusBarComponent} from './status-bar/status-bar.component';
-import {ScreenService} from './services/sceen.service';
+import {ScreenService} from './services/screen.service';
+import {ScreenLarge} from './directives/screen-large.directive';
+import {ScreenBelowLarge} from './directives/screen-below-large.directive';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
-    FrameworkBodyComponent, ContentComponent, TitleBarComponent, TopBarComponent, StatusBarComponent
+    FrameworkBodyComponent, ContentComponent, TitleBarComponent, TopBarComponent, StatusBarComponent, ScreenLarge, ScreenBelowLarge
   ],
   providers: [FrameworkConfigService, ScreenService],
   exports: [FrameworkBodyComponent]
--- src/fw/title-bar/title-bar.component.ts
+++ src/fw/title-bar/title-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ScreenService} from '../services/sceen.service';
+import {ScreenService} from '../services/screen.service';
 
 @Component({
   selector: 'fw-title-bar',
