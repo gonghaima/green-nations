ac0a81029b797034a64396c7dcf7ac1df111f8d8
gonghaima@hotmail.com <5418854188>
2017-07-25 22:45:49 +1200

    Configuration, Styling, and Responsiveness : Creating the Status Bar

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FrameworkBodyComponent} from './framework-body/framework-body.component';
 import {ContentComponent} from './content/content.component';
 import {TitleBarComponent} from './title-bar/title-bar.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {FrameworkConfigService} from './services/framework-config.service';
+import {StatusBarComponent} from './status-bar/status-bar.component';
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [CommonModule],
+  declarations: [
+    FrameworkBodyComponent, ContentComponent, TitleBarComponent, TopBarComponent, StatusBarComponent
   ],
-  declarations: [FrameworkBodyComponent, ContentComponent, TitleBarComponent, TopBarComponent],
-  providers:[FrameworkConfigService],
+  providers: [FrameworkConfigService],
   exports: [FrameworkBodyComponent]
 })
-export class FwModule { }
+export class FwModule {}
