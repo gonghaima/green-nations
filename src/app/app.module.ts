77f6dc3029e0d2d2d355e2dd5ca354b0aefe0aa4
gonghaima@hotmail.com <5418854188>
2017-08-05 14:31:22 +1200

    Advanced Menu Features - Menu Data and Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {FwModule} from '../fw/fw.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {CountriesComponent} from './countries/countries.component';
 import {SettingsComponent} from './settings/settings.component';
 import {appRoutes} from './app.routing';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryMaintComponent } from './country-maint/country-maint.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DashboardComponent, CountriesComponent, SettingsComponent
+    AppComponent, DashboardComponent, SettingsComponent, CountryDetailComponent, CountryListComponent, CountryMaintComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpModule, FwModule, RouterModule.forRoot(appRoutes)
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,16 +2,13 @@ import {Routes} from '@angular/router';
 
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {SettingsComponent} from './settings/settings.component';
-import {CountriesComponent} from './countries/countries.component';
+// import {CountriesComponent} from './countries/countries.component';
 
 export const appRoutes : Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
-  }, {
-    path: 'countries',
-    component: CountriesComponent
-  }, {
+  },  {
     path: 'settings',
     component: SettingsComponent
   }, {
