233557dc58fc5ef01fe366aff8e69b355a315393
gonghaima@hotmail.com <5418854188>
2017-08-06 22:46:11 +1200

    User Controls - Routing and child routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,11 @@ import {appRoutes} from './app.routing';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
 import { CountryListComponent } from './country-list/country-list.component';
 import { CountryMaintComponent } from './country-maint/country-maint.component';
+import { AuthenticatedUserComponent } from './authenticated-user/authenticated-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DashboardComponent, SettingsComponent, CountryDetailComponent, CountryListComponent, CountryMaintComponent
+    AppComponent, DashboardComponent, SettingsComponent, CountryDetailComponent, CountryListComponent, CountryMaintComponent, AuthenticatedUserComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpModule, FwModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthenticatedUserComponent } from './authenticated-user/authenticated-user.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CountryListComponent } from './country-list/country-list.component';
@@ -7,11 +8,15 @@ import { CountryMaintComponent } from './country-maint/country-maint.component';
 import { SettingsComponent } from './settings/settings.component';
 
 export const appRoutes: Routes = [  
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'country-list/:count', component: CountryListComponent },
-  { path: 'country-detail/:country', component: CountryDetailComponent },
-  { path: 'country-maint', component: CountryMaintComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: '', component: DashboardComponent },
-  { path: '**', component: DashboardComponent }
+  { path: 'signin', component: SignInComponent },
+  { path: 'authenticated', component: AuthenticatedUserComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'country-list/:count', component: CountryListComponent },
+      { path: 'country-detail/:country', component: CountryDetailComponent },
+      { path: 'country-maint', component: CountryMaintComponent },
+      { path: 'settings', component: SettingsComponent },
+    ] },
+  { path: '', component: SignInComponent },
+  { path: '**', component: SignInComponent }
 ];
