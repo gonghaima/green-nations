3f4bdf5a6b711e7ceb83344c03fa93e829ede262
gonghaima@hotmail.com <5418854188>
2017-08-06 12:36:59 +1200

    Advanced Menu Features - Routing for Popup Menus

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,21 +1,17 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {SettingsComponent} from './settings/settings.component';
-// import {CountriesComponent} from './countries/countries.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { CountryMaintComponent } from './country-maint/country-maint.component';
+import { SettingsComponent } from './settings/settings.component';
 
-export const appRoutes : Routes = [
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },  {
-    path: 'settings',
-    component: SettingsComponent
-  }, {
-    path: '',
-    component: DashboardComponent
-  }, {
-    path: '**',
-    component: DashboardComponent
-  }
+export const appRoutes: Routes = [  
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'country-list/:count', component: CountryListComponent },
+  { path: 'country-detail/:country', component: CountryDetailComponent },
+  { path: 'country-maint', component: CountryMaintComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '', component: DashboardComponent },
+  { path: '**', component: DashboardComponent }
 ];
