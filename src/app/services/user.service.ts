831cf06ee77a4e1532beea9903d1b5e7a4fda9bf
gonghaima@hotmail.com <5418854188>
2017-08-12 11:24:26 +1200

    User Controls : Signing Out

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
 import { UserApi } from '../../fw/users/user-api';
 
 @Injectable()
 export class UserService implements UserApi {
 
-  isAuthenticated = false;
+  isAuthenticated = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   signIn(username: string, password: string, rememberMe: boolean): Observable<any> {
     console.log('UserService.signIn: ' + username + ' ' + password + ' ' + rememberMe);
     this.isAuthenticated = true;
     return Observable.of({}).delay(2000);
     // return Observable.of({}).delay(2000).flatMap(x=>Observable.throw('Invalid User Name and/or Password'));
+  }
+
+  signOut(): Observable<any> {
+      this.isAuthenticated = false;
+      this.router.navigate(['/signin']);
+      return Observable.of({});
   }
 
 }
--- src/fw/users/user-api.ts
+++ src/fw/users/user-api.ts
@@ -2,6 +2,6 @@ import { Observable } from 'rxjs/Observable';
 
 export abstract class UserApi {
     signIn : (username: string, password: string, rememberMe: boolean) => Observable<any>;
-    // signOut :
+    signOut : ()=>Observable<any>;
     // changePassword : 
 }
