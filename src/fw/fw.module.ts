6679c96ee516770a4bb9f0442e03643edf9cd3cd
gonghaima@hotmail.com <5418854188>
2017-08-13 12:44:53 +1200

    Forms and CRUD Operations : Showing a Country Detail Page

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,27 +1,32 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {FrameworkBodyComponent} from './framework-body/framework-body.component';
-import {ContentComponent} from './content/content.component';
-import {TitleBarComponent} from './title-bar/title-bar.component';
-import {TopBarComponent} from './top-bar/top-bar.component';
-import {FrameworkConfigService} from './services/framework-config.service';
-import {StatusBarComponent} from './status-bar/status-bar.component';
-import {ScreenService} from './services/screen.service';
-import {ScreenLarge} from './directives/screen-large.directive';
-import {ScreenBelowLarge} from './directives/screen-below-large.directive';
-import {MenuService} from './services/menu.service';
-import {MenuComponent} from './menus/menu/menu.component';
-import {MenuItemComponent} from './menus/menu-item/menu-item.component';
-import {PopupMenuComponent} from './menus/popup-menu/popup-menu.component';
-import {SignInComponent} from './users/sign-in/sign-in.component';
-import {RegisterUserComponent} from './users/register-user/register-user.component';
+import { FrameworkBodyComponent } from './framework-body/framework-body.component';
+import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { FrameworkConfigService } from './services/framework-config.service';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { ScreenService } from './services/screen.service';
+import { ScreenLarge } from './directives/screen-large.directive';
+import { ScreenBelowLarge } from './directives/screen-below-large.directive';
+import { MenuService } from './services/menu.service';
+import { MenuComponent } from './menus/menu/menu.component';
+import { MenuItemComponent } from './menus/menu-item/menu-item.component';
+import { PopupMenuComponent } from './menus/popup-menu/popup-menu.component';
+import { SignInComponent } from './users/sign-in/sign-in.component';
+import { RegisterUserComponent } from './users/register-user/register-user.component';
+import { DynamicFormComponent } from './dynamic-forms/dynamic-form/dynamic-form.component';
+import { DynamicFieldComponent } from './dynamic-forms/dynamic-field/dynamic-field.component';
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule, FormsModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     FrameworkBodyComponent,
@@ -35,11 +40,18 @@ import {RegisterUserComponent} from './users/register-user/register-user.compone
     MenuItemComponent,
     PopupMenuComponent,
     SignInComponent,
-    RegisterUserComponent
+    RegisterUserComponent,
+    DynamicFormComponent,
+    DynamicFieldComponent
   ],
   providers: [
-    FrameworkConfigService, ScreenService, MenuService
+    FrameworkConfigService,
+    ScreenService,
+    MenuService
   ],
-  exports: [FrameworkBodyComponent]
+  exports: [
+    FrameworkBodyComponent,
+    DynamicFormComponent
+  ]
 })
-export class FwModule {}
+export class FwModule { }
